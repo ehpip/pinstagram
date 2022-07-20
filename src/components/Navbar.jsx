1527667dc72959278b8fd2abd9c158a7ae4f1309
afif <muhammad.afif5069@gmail.com>
2022-07-20 14:30:11 +0700

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { IoMdAdd, IoMdSearch } from "react-icons/io";
+
+const Navbar = ({ searchTerm, setSearchTerm, user }) => {
+  const navigate = useNavigate();
+
+  if (user) {
+    return (
+      <div className="flex gap-2 md:gap-5 w-full mt-5 pb-7 ">
+        <div className="flex justify-start items-center w-full px-2 rounded-full bg-white border-none outline-none focus-within:shadow-sm hover:shadow-sm">
+          <IoMdSearch fontSize={21} className="ml-1" />
+          <input
+            type="text"
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Search"
+            value={searchTerm}
+            onFocus={() => navigate("/search")}
+            className="p-3 w-full bg-white outline-none rounded-full"
+          />
+        </div>
+        <div className="flex gap-3 ">
+          <Link to={`user-profile/${user?._id}`} className="hidden md:block">
+            <img
+              src={user.image}
+              alt="user-pic"
+              className="w-14 h-12 rounded-full "
+            />
+          </Link>
+          <Link
+            to="/create-pin"
+            className="bg-[#1E2459] text-white rounded-full w-12 h-12 md:w-14 md:h-12 flex justify-center items-center"
+          >
+            <IoMdAdd />
+          </Link>
+        </div>
+      </div>
+    );
+  }
+
+  return null;
+};
+
+export default Navbar;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,80 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { RiHomeFill } from "react-icons/ri";
+import { IoIosArrowForward } from "react-icons/io";
+import logo from "../assets/logo.png";
+import { categories } from "../utils/data";
+
+const isNotActiveStyle =
+  "flex items-center px-5 gap-3 font-bold text-gray-400  hover:text-[#1E2459] hover:bg-blue-300 transition-all duration-200 ease-in-out capitalize py-3";
+const isActiveStyle =
+  "flex items-center px-5 gap-3 font-bold text-[#1E2459] border-r-2 border-[#1E2459]  transition-all duration-200 ease-in-out capitalize py-3";
+
+const Sidebar = ({ closeToggle, user }) => {
+  const handleCloseSidebar = () => {
+    if (closeToggle) closeToggle(false);
+  };
+
+  return (
+    <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar">
+      <div className="flex flex-col">
+        <Link
+          to="/"
+          className="flex px-5 gap-2 my-6 pt-1 w-190 items-center"
+          onClick={handleCloseSidebar}
+        >
+          <img src={logo} alt="logo" className="w-full" />
+        </Link>
+        <div className="flex flex-col">
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? isActiveStyle : isNotActiveStyle
+            }
+            onClick={handleCloseSidebar}
+          >
+            <RiHomeFill />
+            Home
+          </NavLink>
+          <p className="py-3 px-5 2xl:text-xl font-bold">
+            Discover cateogries
+          </p>
+          {categories.slice(0, categories.length - 1).map((category) => (
+            <NavLink
+              to={`/category/${category.name}`}
+              className={({ isActive }) =>
+                isActive ? isActiveStyle : isNotActiveStyle
+              }
+              onClick={handleCloseSidebar}
+              key={category.name}
+            >
+              <img
+                src={category.image}
+                className="w-8 h-8 rounded-full shadow-sm"
+                alt="category"
+              />
+              {category.name}
+            </NavLink>
+          ))}
+        </div>
+      </div>
+      {user && (
+        <Link
+          to={`user-profile/${user._id}`}
+          className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+          onClick={handleCloseSidebar}
+        >
+          <img
+            src={user.image}
+            className="w-10 h-10 rounded-full"
+            alt="user-profile"
+          />
+          <p className="font-bold">{user.userName}</p>
+          <IoIosArrowForward />
+        </Link>
+      )}
+    </div>
+  );
+};
+
+export default Sidebar;
